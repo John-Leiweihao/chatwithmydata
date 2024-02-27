99a66d725589f6118df51b540168f4c1e3ea4330
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-27 13:00:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -39,11 +39,15 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+    messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ]
     # 检查用户输入是否包含"拓扑图"
     if "buck-boost" in prompt:
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = chat_engine.chat(prompt)
+                response = chat_engine.chat(prompt,messages)
                 st.write(response.response)
                 st.image('buck-boost电路.jfif')  # 假设这是与“拓扑图”相关的图片
                 message = {"role": "assistant", "content": response.response}
@@ -52,7 +56,7 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 # 如果用户输入不包含"拓扑图"，执行其他回答或操作
-                response = chat_engine.chat(prompt)
+                response = chat_engine.chat(prompt,messages)
                 st.write(response.response)
                 # 可以在这里添加其他处理逻辑
                 message = {"role": "assistant", "content": response.response}
