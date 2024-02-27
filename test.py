60dc257799b2b790d969cfba1bda77a28b12fe71
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-27 13:36:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,55 +3,9 @@ from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleD
 import os
 import openai
 from llama_index.llms.openai import OpenAI
-from llama_index.core.llms import ChatMessage, MessageRole
-from llama_index.core import ChatPromptTemplate
+from llama_index.core.memory import ChatMemoryBuffer
 
-# Text QA Prompt
-chat_text_qa_msgs = [
-    ChatMessage(
-        role=MessageRole.SYSTEM,
-        content=(
-            "Always answer the question, even if the context isn't helpful."
-        ),
-    ),
-    ChatMessage(
-        role=MessageRole.USER,
-        content=(
-            "Context information is below.\n"
-            "---------------------\n"
-            "{context_str}\n"
-            "---------------------\n"
-            "Given the context information and not prior knowledge, "
-            "answer the question: {query_str}\n"
-        ),
-    ),
-]
-text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
-
-# Refine Prompt
-chat_refine_msgs = [
-    ChatMessage(
-        role=MessageRole.SYSTEM,
-        content=(
-            "Always answer the question, even if the context isn't helpful."
-        ),
-    ),
-    ChatMessage(
-        role=MessageRole.USER,
-        content=(
-            "We have the opportunity to refine the original answer "
-            "(only if needed) with some more context below.\n"
-            "------------\n"
-            "{context_msg}\n"
-            "------------\n"
-            "Given the new context, refine the original answer to better "
-            "answer the question: {query_str}. "
-            "If the context isn't useful, output the original answer again.\n"
-            "Original Answer: {existing_answer}"
-        ),
-    ),
-]
-refine_template = ChatPromptTemplate(chat_refine_msgs)
+memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 api_base = "https://pro.aiskt.com/v1"
@@ -82,8 +36,8 @@ def load_data():
 
 
 index = load_data()
-chat_engine = index.as_chat_engine( text_qa_template=text_qa_template,
-        refine_template=refine_template)
+chat_engine = index.as_chat_engine( chat_mode="context",
+    memory=memory,)
 
 for message in st.session_state.messages:  # Display the prior chat messages
     with st.chat_message(message["role"]):
