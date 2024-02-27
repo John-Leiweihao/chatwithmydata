872c35e6af993abb5afa9f5df4656b1528dbde5c
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-27 11:17:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def load_data():
         return index
 
 index = load_data()
-llm = OpenAI(model="gpt-4-0125-preview", temperature=0)
+llm = OpenAI(model="gpt-4", temperature=0)
 chat_engine = index.as_chat_engine(chat_mode="context",llm=llm) 
 
 for message in st.session_state.messages: # Display the prior chat messages
