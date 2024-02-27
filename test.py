e06d46aa83ac1659ca0d602bf55312d9419b4bda
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-27 13:15:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,64 @@ from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleD
 import os
 import openai
 from llama_index.llms.openai import OpenAI
+from llama_index.llms import ChatMessage, MessageRole
+from llama_index.prompts import ChatPromptTemplate
+
+# Text QA Prompt
+chat_text_qa_msgs = [
+    ChatMessage(
+        role=MessageRole.SYSTEM,
+        content=(
+            "You are an expert Q&A system that is trusted around the world.\n"
+            "Always answer the query using the provided context information, "
+            "and not prior knowledge.\n"
+            "Some rules to follow:\n"
+            "1. Never directly reference the given context in your answer.\n"
+            "2. Avoid statements like 'Based on the context, ...' or "
+            "'The context information ...' or anything along "
+            "those lines."
+        ),
+    ),
+    ChatMessage(
+        role=MessageRole.USER,
+        content=(
+            "Context information is below.\n"
+            "---------------------\n"
+            "{context_str}\n"
+            "---------------------\n"
+            "Given the context information and not prior knowledge, "
+            "answer the query.\n"
+            "Query: {query_str}\n"
+            "Answer: "
+        ),
+    ),
+]
+text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
+
+# Refine Prompt
+chat_refine_msgs = [
+    ChatMessage(
+        role=MessageRole.SYSTEM,
+        content=(
+            "You are an expert Q&A system that strictly operates in two modes "
+            "when refining existing answers:\n"
+            "1. **Rewrite** an original answer using the new context.\n"
+            "2. **Repeat** the original answer if the new context isn't useful.\n"
+            "Never reference the original answer or context directly in your answer.\n"
+            "When in doubt, just repeat the original answer."
+        ),
+    ),
+    ChatMessage(
+        role=MessageRole.USER,
+        content=(
+            "New Context: {context_msg}\n"
+            "Query: {query_str}\n"
+            "Original Answer: {existing_answer}\n"
+            "New Answer: "
+        ),
+    ),
+]
+refine_template = ChatPromptTemplate(chat_refine_msgs)
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 api_base = "https://pro.aiskt.com/v1"
@@ -33,7 +91,7 @@ def load_data():
 
 
 index = load_data()
-chat_engine = index.as_chat_engine(chat_mode="context", verbose=True)
+chat_engine = index.as_chat_engine(text_qa_template=text_qa_template, refine_template=refine_template)
 
 for message in st.session_state.messages:  # Display the prior chat messages
     with st.chat_message(message["role"]):
