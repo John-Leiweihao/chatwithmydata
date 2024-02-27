68ba43060996dfa6d8bd9aa8b40ae75715daef77
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-27 17:47:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -65,7 +65,7 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
                 # 如果用户输入不包含"拓扑图"，执行其他回答或操作
                 response = chat_engine.chat(prompt,messages_history)
                 answer_list=response.response
-                Uin=answer_list[0],Uo=answer_list[1],Prated=answer_list[2],fsw=answer_list[3]
+                Uin, Uo, Prated, fsw = answer_list
                 M,L1, Cin1, Cout1=twolevelbuckboost.calculation(Uin,Uo,Prated,fsw)
                 reply="这个电路工作在{}模态，其中电路的电感值为{},输入电容值为{},输出电容值为{}.".format(M,L1,Cin1,Cout1)
                 st.write(reply)
