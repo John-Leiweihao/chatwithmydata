7d8d8c45d3d0eff4200f4d524a49a531de290eaa
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-27 10:50:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -24,12 +24,12 @@ if "messages" not in st.session_state: # Initialize the chat messages history
 def load_data():
     with st.spinner(text="Loading and indexing the buck-boost docs – hang tight! This should take 1-2 minutes."):
         docs = SimpleDirectoryReader("data1").load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0125-preview", temperature=0.1))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0125-preview", temperature=0))
         index = GPTVectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
-chat_engine = index.as_chat_engine(chat_mode="context",memory=memory) 
+chat_engine = index.as_chat_engine(memory=memory) 
 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
