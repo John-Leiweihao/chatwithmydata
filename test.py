dd138ea796fb1ab705681fd732941566d4c818ed
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-29 19:53:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 api_base = "https://pro.aiskt.com/v1"
 openai.base_url = api_base
 
-st.set_page_config(page_title="Chat with the Power electronic robot", page_icon="🤖", layout="centered",
-                   initial_sidebar_state="expanded", menu_items=None)
+st.set_page_config(page_title="Chat with the Power electronic robot", page_icon="🏫", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
 st.title("Chat with the Power electronic robot🤖, powered by LlamaIndex 🙂")
 st.info( "Hello, I am a robot designed specifically for buck-boost circuits!", icon="🤟")
 with open('./second.txt', 'r') as file:
