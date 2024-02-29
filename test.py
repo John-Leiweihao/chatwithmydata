834f2eae93499286e38b43b7c6d764b1f55934e4
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-02-29 17:27:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from llama_index.llms.openai import OpenAI
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.llms import ChatMessage, MessageRole
 import twolevelbuckboost
+import threelevelbuckboost
 import ast
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 api_base = "https://pro.aiskt.com/v1"
@@ -51,15 +52,25 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
     for m in st.session_state.messages
 ]
     # 检查用户输入是否包含"拓扑图"
-    if "buck-boost" in prompt:
+    if "two-level buck-boost" in prompt:
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 response = chat_engine.chat(prompt,messages_history)
                 st.write(response.response)
+                st.write("The topology of the two-level buck-boost circuit is shown in the following figure")
                 st.image('buck-boost电路.jfif')  # 假设这是与“拓扑图”相关的图片
                 message = {"role": "assistant", "content": response.response}
                 st.session_state.messages.append(message)
-    elif all(param in prompt for param in ["Uin", "Uo", "Prated", "fsw"]):
+    elif "three-level buck-boost" in prompt:
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = chat_engine.chat(prompt,messages_history)
+                st.write(response.response)
+                st.write("The topology of the three-level buck-boost circuit is shown in the following figure")
+                st.image('buck-boost电路.jfif')  # 假设这是与“拓扑图”相关的图片
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message)
+    elif all(param in prompt for param in ["Uin", "Uo", "Prated", "fsw","two-level"]):
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 # 如果用户输入不包含"拓扑图"，执行其他回答或操作
@@ -67,9 +78,25 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
                 answer_list = ast.literal_eval(response.response)
                # st.write(answer_list)
                 Uin, Uo, Prated, fsw = answer_list
-                M,L1, Cin1, Cout1=twolevelbuckboost.calculation(Uin,Uo,Prated,fsw)
-                reply="这个电路工作在{}模态，其中电路的电感值为{},输入电容值为{},输出电容值为{}.".format(M,L1,Cin1,Cout1)
+                M,L1, Cin1, Cout1,R,KP,KI=twolevelbuckboost.calculation2(Uin,Uo,Prated,fsw)
+                reply="这个两电平buck-boost电路工作在{}模态，其中电路的电感值为{},输入电容值为{},输出电容值为{},负载电阻值为{}.对于这款电源，我推荐您使用PI控制器，控制器的框图如下图所示，其中KP值为{}，KI值为{}.".format(M,L1,Cin1,Cout1,R,KP,KI)
                 st.write(reply)
+                st.image()
+                # 可以在这里添加其他处理逻辑
+                message = {"role": "assistant", "content": reply}
+                st.session_state.messages.append(message)
+    elif all(param in prompt for param in ["Uin", "Uo", "Prated", "fsw","three-level"]):
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                # 如果用户输入不包含"拓扑图"，执行其他回答或操作
+                response = chat_engine.chat(prompt,messages_history)
+                answer_list = ast.literal_eval(response.response)
+               # st.write(answer_list)
+                Uin, Uo, Prated, fsw = answer_list
+                M,L1, Cin1, Cout1,R,KP,KI=twolevelbuckboost.calculation3(Uin,Uo,Prated,fsw)
+                reply="这个三电平buck-boost电路工作在{}模态，其中电路的电感值为{},输入电容值为{},输出电容值为{},负载电阻值为{}.对于这款电源，我推荐您使用PI控制器，控制器的框图如下图所示，其中KP值为{}，KI值为{}.".format(M,L1,Cin1,Cout1,R,KP,KI)
+                st.write(reply)
+                st.image()
                 # 可以在这里添加其他处理逻辑
                 message = {"role": "assistant", "content": reply}
                 st.session_state.messages.append(message)
